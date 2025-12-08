eec5b2ce82646d561d85be69ef48d0855c9a1c46
TylerParrack <tap101@pitt.edu>
2025-12-08 09:35:59 -0500

    PP Fit Fuctions
    Fitting functions and whatnot to get pump probe data. I basically just adjusted the non-PP files.

---
+++ OO_workspace/cropDataPP.m
@@ -0,0 +1,56 @@
+function [dataobj] = cropDataPP(data,range3)
+% CROPDATA returns a cropped section of 2D-IR data.
+%
+%   CROPPEDDATAOBJ = cropData(DATASTRUCT,w1_range,w3_range)
+%   creates a 1xn data structure array, DO, the
+%   contains a cropped version of the w1, w3, and response values from
+%   2D-IR data (based on the input ranges specified), as well as the t2
+%   times.
+%
+%   --DATASTRUCT must be a a 1xn 2D-IR structure array with the fields
+%   data(n).w1,data(n).w3, data(n).R (the response), data(n).t2 (population
+%   times)
+%
+%   --w1_range should be the w1 range in cm-1 [w1(1) w1(end)]
+%   --w3_range should be the w3 range in cm-1 [w3(1) w3(end)]
+%
+%   This function was designed to allow (1) the ability to omit indices
+%   from the fitting functions we use by cropping data to the desired size
+%   initially (or in the course of the function), and (2) to reduce the
+%   size of the data object being passed through successive methods in
+%   fitting, since the resulting data object is approximately 0.1% of the
+%   size of the original data array for typical data.
+
+
+dataobj = struct('data',[],'freq',[]);
+for ii = 1:numel(data)
+    if isempty(data(ii).data)
+        continue
+    end
+    
+    % Allow the use of empty ranges to not crop along specific dimensions
+    if isempty(range3)
+        range3 = [data(ii).freq(1) data(ii).freq(end)];
+    end
+    ind3 = data(ii).freq>=range3(1) & data(ii).freq<=range3(2);
+    dataobj(ii).freq = data(ii).freq(ind3);
+    dataobj(ii).data = data(ii).data(ind3);
+    dataobj(ii).t2 = data(ii).t2;
+    if isfield(data,'PARAMS')
+        dataobj(ii).PARAMS = data(ii).PARAMS;
+    end
+    if isfield(data,'phase')
+        dataobj(ii).phase = data(ii).phase;
+    end
+    if isfield(data,'scan_number')
+        dataobj(ii).scan_number = data(ii).scan_number;
+    end
+    if isfield(data,'datestring')
+        dataobj(ii).datestring = data(ii).datestring;
+    end
+    if isfield(data,'spec_calib')
+        dataobj(ii).spec_calib = data(ii).spec_calib;
+    end
+end
+
+dataobj = reshape(dataobj,size(data));
---
+++ OO_workspace/extractMaximaPP.m
@@ -0,0 +1,103 @@
+function [out] = extractMaximaPP(dataobj,options)
+% fitStruct = extractMaxima(dataobj,options)
+%
+% extractMaxima attempts to fit the peak shape of slices of a 2D-IR
+% spectrum for use in centerline slope analysis. Input (dataobj) is assumed
+% to be (cropped) 2D-IR data, which is further cropped for fitting in this
+% function. For each spectrum, this code attempts to fit slices along
+% omega_3 with the inputted fitting function (normally a sum of Gaussians
+% or Voigt profiles). The function returns the data, fit result, goodness
+% of fit, and fitinfo in an n x m structure, where n is the number of t2
+% points, and m is the number of omega_1 values fitted.
+%
+%   required fields for 'options'
+%       'range1' - the omega_1 fitting range
+%       'range3' - the omega_3 fitting range
+%       'fitfcn' - the fitting function
+%       'startpoint' - the starting point of the fit 
+%       'lb' - the lower bound of the fit
+%       'ub' - the upper bound of the fit
+%
+%   possible additional fields
+%       'estimateArea' - (default false) if true, attempts to estimate the
+%       area of the peak by calling 'estimatePeakArea.m', and replaces the
+%       amplitude (assumed format 'a\d') values in the fitting bounds based
+%       on the area. Useful when using a normalized fitting function (like
+%       our Voigt profile) 
+%
+%       'fitPosPeak' - (default false) specifies the behavior of
+%       estimatePeakArea. If true, it will attempt to fit a positive peak
+%       (probably the ESA). If false, it will fit a negative peak (GSB/SE).
+%
+%       'flag_plot' - (default false) If true, plots fitting results as
+%       they come in.
+%
+%       'estimate_flag_plot' - (default false) similar to flag_plot, but
+%       for estimatePeakArea
+%
+% TODO: Allow more control over relative magnitudes in estimateArea
+
+% default values
+flag_plot = 0;
+estimateArea = 0;
+fitPosPeak = 0;
+peakAreaScale = 2;
+
+% checking the fields that the user passed to the function to ensure that
+% they match expected fields
+required_fields = {'range3' 'fitfcn' 'flag_plot' 'lb' 'startpoint' 'ub'};
+possible_fields = {'estimateArea' 'estimate_flag_plot' 'fitPosPeak' 'fitfcn' 'flag_plot' 'lb' 'startpoint' 'ub','peakAreaScale'};
+given_fields = fields(options);
+missing_fields = setdiff(sort(required_fields),sort(given_fields));
+unexpected_fields = setdiff(sort(given_fields),sort(possible_fields));
+if ~isempty(missing_fields)
+    error('Required options fields not found.');
+end
+if ~isempty(unexpected_fields)
+    warning('Unexpected options fields.')
+end
+
+% unpacking the options structrue into a series of variables 
+for ji=1:length(given_fields)
+    eval([given_fields{ji} '=options.' given_fields{ji} ]);
+end
+
+out = struct('fitresult',[],'gof',[],'fitinfo',[]);
+for ii = 1:length(dataobj)
+    w3 = dataobj(ii).freq;
+    w3a = min(w3):0.1:max(w3);
+    R = dataobj(ii).data;
+        % redefining our starting parameters for each spectrum slice.
+        if estimateArea
+            area = estimatePeakArea(w3,R,'fitPosPeak',fitPosPeak,'flag_plot',estimate_flag_plot);
+            % got rid of hard coding -- now amplitudes can be located
+            % anywhere in the bounds, and there can be any number of
+            % amplitudes.
+            coefficients = coeffnames(fitfcn);
+            aTest = regexp(coefficients,'a\d'); % find the pattern 'a#'
+            ind = ~cellfun(@isempty,aTest); % find where those are living
+            startpoint(ind) = area; % redefine our startpoints
+            lb(ind) = area/peakAreaScale;
+            ub(ind) = peakAreaScale*area;
+        end
+        % the fitting part
+  [fitresult,gof,fitinfo] = fit(w3(:),R(:),fitfcn, 'StartPoint',startpoint,...
+       'lower',lb,'upper',ub);
+  out(ii).fitresult = fitresult;
+  out(ii).gof = gof;
+  out(ii).fitinfo = fitinfo;
+  out(ii).w3 = w3;
+  out(ii).R = R(:);
+  if flag_plot 
+           fig = figure(1);
+           fig.Color = 'w';
+           clf
+           p1 = plot(w3(:),R(:),'b.',w3a,fitresult(w3a));
+           fig.Children.Box = 'off';
+           fig.Children.TickDir = 'out';
+           title(sprintf('%i fs',dataobj(ii).t2));
+           xlabel('Frequency (cm^{-1})')
+           ylabel('\DeltaA (mOD)')
+           pause(0.5)
+   end       
+end
